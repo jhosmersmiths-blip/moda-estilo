d85a3172301343bf3b7b81f3be87e0cd6c747a28
JHOSMER <JHOSMER@JSSS>
2026-05-31 22:26:28 -0500

    iniciando proyecto

---
+++ src/java/Model/Usuario.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author JHOSMER
+ */
+public class Usuario {
+    private int id_usuario;
+    private String usuario;
+    private String contraseña;
+    private Rol rol;
+    private Persona persona;
+
+    public Usuario() {
+    }
+
+    public Usuario(int id_usuario, String usuario, String contraseña, Rol rol, Persona persona) {
+        this.id_usuario = id_usuario;
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+        this.rol = rol;
+        this.persona = persona;
+    }
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
+    public Persona getPersona() {
+        return persona;
+    }
+
+    public void setPersona(Persona persona) {
+        this.persona = persona;
+    }
+    
+    
+}
